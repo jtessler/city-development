70970b5bf08cd7b1daddf493fbb840261008d284
Joe Tessler <joe.r.tessler@gmail.com>
2012-11-26 19:51:36 -0600

    Added a triangle mesh.

---
+++ cidev/webgl/triangle.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview A triangle mesh.
+ *
+ * @author joseph@cs.utexas.edu (Joe Tessler)
+ */
+
+goog.provide("cidev.webgl.Triangle");
+
+goog.require("goog.webgl");
+
+/**
+ * @param {!cidev.webgl.Context} context The WebGL context wrapper.
+ * @constructor
+ */
+cidev.webgl.Triangle = function(context) {
+  var gl = context.gl;
+
+  /** @type {WebGLBuffer} */
+  this.vertexBuffer = gl.createBuffer();
+
+  gl.bindBuffer(goog.webgl.ARRAY_BUFFER, this.vertexBuffer);
+  var vertices = [
+       0.0,  1.0,  0.0,
+      -1.0, -1.0,  0.0,
+       1.0, -1.0,  0.0
+  ];
+  gl.bufferData(goog.webgl.ARRAY_BUFFER, new Float32Array(vertices),
+      goog.webgl.STATIC_DRAW);
+  this.vertexBuffer.itemSize = 3;
+  this.vertexBuffer.numItems = 3;
+}
